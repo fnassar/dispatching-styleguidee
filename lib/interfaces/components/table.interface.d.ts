c26a69c3ab7cea59ce4ad96f3705fed5ff7f4cae
Fatema Nassar <fnassar@fpconsultancy.com>
2025-06-16 13:48:56 +0300

    update style guide

--- lib/interfaces/components/table.interface.d.ts
+++ lib/interfaces/components/table.interface.d.ts
@@ -1,25 +1,23 @@
-import { ElementRef } from '@angular/core';
-
 export interface ITableHeader<T> {
-  header: string;
-  body?: keyof T;
-  clickFn?: (value: T) => void;
-  htmlRef?: ElementRef;
-  sort: boolean;
-  type?: 'Status' | 'Actions' | undefined;
+    header: string;
+    body?: keyof T;
+    clickFn?: (value: T) => void;
+    htmlRef?: string;
+    sort: boolean;
+    type?: 'Status' | 'Actions' | undefined;
 }
 export interface ITableCategory<T> {
-  category: string;
-  rows: T[];
-  isCollapsed?: boolean;
+    category: string;
+    rows: T[];
+    isCollapsed?: boolean;
 }
 export interface IPageChangeEvent {
-  page: number;
-  pageSize: number;
+    page: number;
+    pageSize: number;
 }
 export interface IStatusCol<T> {
-  header: keyof T;
-  trueValue: string | boolean;
-  trueText: string;
-  falseText: string;
+    header: keyof T;
+    trueValue: string | boolean;
+    trueText: string;
+    falseText: string;
 }
