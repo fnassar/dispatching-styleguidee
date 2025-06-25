3e59a36db6fcd12edc9fa035541c5ed2a5bc0a8c
Fatema Nassar <fnassar@fpconsultancy.com>
2025-06-25 11:36:27 +0300

    update modal

--- lib/components/custom-modal/custom-modal.component.d.ts
+++ lib/components/custom-modal/custom-modal.component.d.ts
@@ -4,6 +4,7 @@ export declare class CustomModalComponent {
     modalTitle: string;
     showDot: boolean;
     headerButton: string;
+    overlayClickClose: boolean;
     headerButtonClick: EventEmitter<void>;
     isVisible: boolean;
     open(): void;
@@ -11,5 +12,5 @@ export declare class CustomModalComponent {
     onHeaderButtonClick(): void;
     onOverlayClick(event: MouseEvent): void;
     static ɵfac: i0.ɵɵFactoryDeclaration<CustomModalComponent, never>;
-    static ɵcmp: i0.ɵɵComponentDeclaration<CustomModalComponent, "modal", never, { "modalTitle": { "alias": "modalTitle"; "required": false; }; "showDot": { "alias": "showDot"; "required": false; }; "headerButton": { "alias": "headerButton"; "required": false; }; }, { "headerButtonClick": "headerButtonClick"; }, never, ["*"], true, never>;
+    static ɵcmp: i0.ɵɵComponentDeclaration<CustomModalComponent, "modal", never, { "modalTitle": { "alias": "modalTitle"; "required": false; }; "showDot": { "alias": "showDot"; "required": false; }; "headerButton": { "alias": "headerButton"; "required": false; }; "overlayClickClose": { "alias": "overlayClickClose"; "required": false; }; }, { "headerButtonClick": "headerButtonClick"; }, never, ["*"], true, never>;
 }
