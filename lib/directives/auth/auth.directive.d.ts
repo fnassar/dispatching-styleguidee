672364a159b16ba85d161769311fc76d82d7ab7c
Fatema Nassar <fnassar@fpconsultancy.com>
2025-07-09 12:47:00 +0300

    update auth directive and permission guard

--- lib/directives/auth/auth.directive.d.ts
+++ lib/directives/auth/auth.directive.d.ts
@@ -1,12 +1,13 @@
 import { TemplateRef, ViewContainerRef } from '@angular/core';
-import { AuthService } from '../../services';
+import { AuthContextService, AuthService } from '../../services';
 import * as i0 from "@angular/core";
 export declare class AuthDirective {
     private authService;
     private templateRef;
     private viewContainer;
-    constructor(authService: AuthService, templateRef: TemplateRef<any>, viewContainer: ViewContainerRef);
-    set canDoAction(actions: string[]);
+    private authContextService;
+    readonly actions: import("@angular/core").InputSignal<string[]>;
+    constructor(authService: AuthService, templateRef: TemplateRef<any>, viewContainer: ViewContainerRef, authContextService: AuthContextService);
     static ɵfac: i0.ɵɵFactoryDeclaration<AuthDirective, never>;
-    static ɵdir: i0.ɵɵDirectiveDeclaration<AuthDirective, "[canDoAction]", never, { "canDoAction": { "alias": "canDoAction"; "required": false; }; }, {}, never, never, true, never>;
+    static ɵdir: i0.ɵɵDirectiveDeclaration<AuthDirective, "[canDoAction]", never, { "actions": { "alias": "actions"; "required": false; "isSignal": true; }; }, {}, never, never, true, never>;
 }
--- lib/guards/auth/permission.guard.d.ts
+++ lib/guards/auth/permission.guard.d.ts
@@ -1,11 +1,2 @@
-import { ActivatedRouteSnapshot, CanActivate, Router } from '@angular/router';
-import { AuthService } from '../../services';
-import * as i0 from "@angular/core";
-export declare class PermissionGuard implements CanActivate {
-    private router;
-    private authService;
-    constructor(router: Router, authService: AuthService);
-    canActivate(route: ActivatedRouteSnapshot): boolean;
-    static ɵfac: i0.ɵɵFactoryDeclaration<PermissionGuard, never>;
-    static ɵprov: i0.ɵɵInjectableDeclaration<PermissionGuard>;
-}
+import { CanActivateFn } from '@angular/router';
+export declare const PermissionGuard: CanActivateFn;
