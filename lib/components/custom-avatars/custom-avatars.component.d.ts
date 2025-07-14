990a4a0b48c455c37c2d95c3cf8c3bfd7e8d16f0
Fatema Nassar <fnassar@fpconsultancy.com>
2025-07-15 01:17:59 +0300

    add input to custom avatars

--- lib/components/custom-avatars/custom-avatars.component.d.ts
+++ lib/components/custom-avatars/custom-avatars.component.d.ts
@@ -7,8 +7,9 @@ export declare class CustomAvatarsComponent {
     items: AvatarItem[];
     size: number;
     overlapOffset: number;
+    removeOutline: boolean;
     getInitials(name: string): string;
     getRandomColor(): string;
     static ɵfac: i0.ɵɵFactoryDeclaration<CustomAvatarsComponent, never>;
-    static ɵcmp: i0.ɵɵComponentDeclaration<CustomAvatarsComponent, "custom-avatars", never, { "items": { "alias": "items"; "required": false; }; "size": { "alias": "size"; "required": false; }; "overlapOffset": { "alias": "overlapOffset"; "required": false; }; }, {}, never, never, true, never>;
+    static ɵcmp: i0.ɵɵComponentDeclaration<CustomAvatarsComponent, "custom-avatars", never, { "items": { "alias": "items"; "required": false; }; "size": { "alias": "size"; "required": false; }; "overlapOffset": { "alias": "overlapOffset"; "required": false; }; "removeOutline": { "alias": "removeOutline"; "required": false; }; }, {}, never, never, true, never>;
 }
