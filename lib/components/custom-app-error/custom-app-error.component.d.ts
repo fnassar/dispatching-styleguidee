4833ce0769ad617216dde9c9a13934e03774b18f
AbdelrahmanSamir <55239369+abdelrahman719@users.noreply.github.com>
2025-07-22 04:04:03 +0300

    fiex design issues --zalaky-comments

--- lib/components/custom-app-error/custom-app-error.component.d.ts
+++ lib/components/custom-app-error/custom-app-error.component.d.ts
@@ -8,6 +8,7 @@ export declare class CustomAppErrorComponent {
     showErrors: import("@angular/core").InputSignal<boolean>;
     shouldShowError(item: IComponentFormError): boolean;
     getErrorMessage(item: IComponentFormError): string;
+    hasAnyErrorToShow(): boolean;
     static ɵfac: i0.ɵɵFactoryDeclaration<CustomAppErrorComponent, never>;
     static ɵcmp: i0.ɵɵComponentDeclaration<CustomAppErrorComponent, "custom-app-error", never, { "control": { "alias": "control"; "required": true; }; "validation": { "alias": "validation"; "required": true; }; "name": { "alias": "name"; "required": true; }; "showErrors": { "alias": "showErrors"; "required": false; "isSignal": true; }; }, {}, never, never, true, never>;
 }
