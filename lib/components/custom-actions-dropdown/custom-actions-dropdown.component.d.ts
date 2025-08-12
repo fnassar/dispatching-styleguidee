ce84bc43fc5c3f1e46e457698795a83c0f14fc59
AbdelrahmanSamir <55239369+abdelrahman719@users.noreply.github.com>
2025-08-12 13:21:23 +0300

    deploy for lib updates

--- lib/components/custom-actions-dropdown/custom-actions-dropdown.component.d.ts
+++ lib/components/custom-actions-dropdown/custom-actions-dropdown.component.d.ts
@@ -16,7 +16,7 @@ export declare class CustomActionsDropdownComponent {
     closeDropdown(): void;
     onClickAction(action: {
         callback: (ctx: any) => void;
-    }): void;
+    }, event: MouseEvent): void;
     static ɵfac: i0.ɵɵFactoryDeclaration<CustomActionsDropdownComponent, never>;
     static ɵcmp: i0.ɵɵComponentDeclaration<CustomActionsDropdownComponent, "custom-actions-dropdown", never, { "actions": { "alias": "actions"; "required": false; }; "context": { "alias": "context"; "required": false; }; "horizontalDots": { "alias": "horizontalDots"; "required": false; }; }, {}, never, never, true, never>;
 }
