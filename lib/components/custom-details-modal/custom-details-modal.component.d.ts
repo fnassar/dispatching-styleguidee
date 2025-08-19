9ae39b3d343a3232238245b1598c26b1366f1dc8
AbdelrahmanSamir <55239369+abdelrahman719@users.noreply.github.com>
2025-08-19 09:04:57 +0300

    deploy for custom popup

--- lib/components/custom-details-modal/custom-details-modal.component.d.ts
+++ lib/components/custom-details-modal/custom-details-modal.component.d.ts
@@ -13,9 +13,12 @@ export declare class CustomDetailsModalComponent {
     actionsDropdown: import("@angular/core").InputSignal<IDropdownOption[]>;
     dropdownSelectAction: EventEmitter<string | number>;
     isVisible: boolean;
+    mainSize: number;
+    sideSize: number;
+    ngOnInit(): void;
     open(): void;
     close(): void;
     onOverlayClick(event: MouseEvent): void;
     static ɵfac: i0.ɵɵFactoryDeclaration<CustomDetailsModalComponent, never>;
-    static ɵcmp: i0.ɵɵComponentDeclaration<CustomDetailsModalComponent, "custom-details-modal", never, { "modalTitle": { "alias": "modalTitle"; "required": false; }; "overlayClickClose": { "alias": "overlayClickClose"; "required": false; }; "BreadCrumbs": { "alias": "BreadCrumbs"; "required": true; }; "sectionDimensions": { "alias": "sectionDimensions"; "required": false; }; "actionsDropdown": { "alias": "actionsDropdown"; "required": false; "isSignal": true; }; }, { "headerButtonClick": "headerButtonClick"; "dropdownSelectAction": "dropdownSelectAction"; }, never, ["[mainContent]", "[sideContent]"], true, never>;
+    static ɵcmp: i0.ɵɵComponentDeclaration<CustomDetailsModalComponent, "custom-details-modal", never, { "modalTitle": { "alias": "modalTitle"; "required": false; }; "overlayClickClose": { "alias": "overlayClickClose"; "required": false; }; "BreadCrumbs": { "alias": "BreadCrumbs"; "required": true; }; "sectionDimensions": { "alias": "sectionDimensions"; "required": false; }; "actionsDropdown": { "alias": "actionsDropdown"; "required": false; "isSignal": true; }; "mainSize": { "alias": "mainSize"; "required": false; }; "sideSize": { "alias": "sideSize"; "required": false; }; }, { "headerButtonClick": "headerButtonClick"; "dropdownSelectAction": "dropdownSelectAction"; }, never, ["[mainContent]", "[sideContent]"], true, never>;
 }
