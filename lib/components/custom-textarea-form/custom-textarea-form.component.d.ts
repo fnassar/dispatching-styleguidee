a880ed8cf3273b9031b0233bd8abb6d287613024
Fatema Nassar <fnassar@fpconsultancy.com>
2025-10-10 19:29:29 +0300

    fix: update error before leaving input focus

--- lib/components/custom-textarea-form/custom-textarea-form.component.d.ts
+++ lib/components/custom-textarea-form/custom-textarea-form.component.d.ts
@@ -1,3 +1,4 @@
+import { EventEmitter } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { IComponentFormError } from '../../interfaces';
 import * as i0 from "@angular/core";
@@ -14,7 +15,10 @@ export declare class CustomTextareaFormComponent {
     validation: IComponentFormError[];
     viewType: 'base' | 'ai-plan';
     row: string;
+    disabled: boolean;
+    valueChange: EventEmitter<string>;
     containRequiredError(): boolean;
+    onValueChange(): void;
     static ɵfac: i0.ɵɵFactoryDeclaration<CustomTextareaFormComponent, never>;
-    static ɵcmp: i0.ɵɵComponentDeclaration<CustomTextareaFormComponent, "custom-textarea-form", never, { "class": { "alias": "class"; "required": false; }; "labelClass": { "alias": "labelClass"; "required": false; }; "label": { "alias": "label"; "required": false; }; "placeholder": { "alias": "placeholder"; "required": false; }; "resizable": { "alias": "resizable"; "required": false; "isSignal": true; }; "customHeight": { "alias": "customHeight"; "required": false; "isSignal": true; }; "name": { "alias": "name"; "required": true; }; "controlName": { "alias": "controlName"; "required": true; }; "parentForm": { "alias": "parentForm"; "required": true; }; "validation": { "alias": "validation"; "required": true; }; "viewType": { "alias": "viewType"; "required": false; }; "row": { "alias": "row"; "required": false; }; }, {}, never, never, true, never>;
+    static ɵcmp: i0.ɵɵComponentDeclaration<CustomTextareaFormComponent, "custom-textarea-form", never, { "class": { "alias": "class"; "required": false; }; "labelClass": { "alias": "labelClass"; "required": false; }; "label": { "alias": "label"; "required": false; }; "placeholder": { "alias": "placeholder"; "required": false; }; "resizable": { "alias": "resizable"; "required": false; "isSignal": true; }; "customHeight": { "alias": "customHeight"; "required": false; "isSignal": true; }; "name": { "alias": "name"; "required": true; }; "controlName": { "alias": "controlName"; "required": true; }; "parentForm": { "alias": "parentForm"; "required": true; }; "validation": { "alias": "validation"; "required": true; }; "viewType": { "alias": "viewType"; "required": false; }; "row": { "alias": "row"; "required": false; }; "disabled": { "alias": "disabled"; "required": false; }; }, { "valueChange": "valueChange"; }, never, never, true, never>;
 }
