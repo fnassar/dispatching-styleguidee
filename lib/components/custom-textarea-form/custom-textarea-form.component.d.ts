3f6d533781d15c9eda0e4b31491efaf2e6e22525
Fatema Nassar <fnassar@fpconsultancy.com>
2025-10-27 15:49:53 +0300

    enforce input max -- not just error

--- lib/components/custom-textarea-form/custom-textarea-form.component.d.ts
+++ lib/components/custom-textarea-form/custom-textarea-form.component.d.ts
@@ -19,6 +19,8 @@ export declare class CustomTextareaFormComponent {
     valueChange: EventEmitter<string>;
     containRequiredError(): boolean;
     onValueChange(): void;
+    getMaxLength(): number | null;
+    getMinLength(): number | null;
     static ɵfac: i0.ɵɵFactoryDeclaration<CustomTextareaFormComponent, never>;
     static ɵcmp: i0.ɵɵComponentDeclaration<CustomTextareaFormComponent, "custom-textarea-form", never, { "class": { "alias": "class"; "required": false; }; "labelClass": { "alias": "labelClass"; "required": false; }; "label": { "alias": "label"; "required": false; }; "placeholder": { "alias": "placeholder"; "required": false; }; "resizable": { "alias": "resizable"; "required": false; "isSignal": true; }; "customHeight": { "alias": "customHeight"; "required": false; "isSignal": true; }; "name": { "alias": "name"; "required": true; }; "controlName": { "alias": "controlName"; "required": true; }; "parentForm": { "alias": "parentForm"; "required": true; }; "validation": { "alias": "validation"; "required": true; }; "viewType": { "alias": "viewType"; "required": false; }; "row": { "alias": "row"; "required": false; }; "disabled": { "alias": "disabled"; "required": false; }; }, { "valueChange": "valueChange"; }, never, never, true, never>;
 }
