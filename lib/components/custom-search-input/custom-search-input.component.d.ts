4b70f3c069a471b107d1fdf09905991a8124eb9e
Fatema Nassar <fnassar@fpconsultancy.com>
2025-11-13 14:48:52 +0200

    update search components

--- lib/components/custom-search-input/custom-search-input.component.d.ts
+++ lib/components/custom-search-input/custom-search-input.component.d.ts
@@ -8,7 +8,12 @@ export declare class CustomSearchInputComponent {
     name: string;
     value: any;
     style: 'base' | 'white' | 'grey';
-    valueChange: EventEmitter<any>;
+    modelChange: EventEmitter<string>;
+    search: EventEmitter<string>;
+    valueChange: EventEmitter<string>;
+    private inputSubject;
+    constructor();
+    onInputChange(value: string): void;
     static ɵfac: i0.ɵɵFactoryDeclaration<CustomSearchInputComponent, never>;
-    static ɵcmp: i0.ɵɵComponentDeclaration<CustomSearchInputComponent, "custom-search-input", never, { "class": { "alias": "class"; "required": false; }; "labelClass": { "alias": "labelClass"; "required": false; }; "label": { "alias": "label"; "required": false; }; "placeholder": { "alias": "placeholder"; "required": false; }; "name": { "alias": "name"; "required": false; }; "value": { "alias": "value"; "required": true; }; "style": { "alias": "style"; "required": false; }; }, { "valueChange": "valueChange"; }, never, never, true, never>;
+    static ɵcmp: i0.ɵɵComponentDeclaration<CustomSearchInputComponent, "custom-search-input", never, { "class": { "alias": "class"; "required": false; }; "labelClass": { "alias": "labelClass"; "required": false; }; "label": { "alias": "label"; "required": false; }; "placeholder": { "alias": "placeholder"; "required": false; }; "name": { "alias": "name"; "required": false; }; "value": { "alias": "value"; "required": true; }; "style": { "alias": "style"; "required": false; }; }, { "modelChange": "modelChange"; "search": "search"; "valueChange": "valueChange"; }, never, never, true, never>;
 }
