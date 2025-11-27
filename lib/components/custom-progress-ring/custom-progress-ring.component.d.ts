7a606826d3f2a685aad3b80d02c20907dfbed95c
Fatema <fan6236@nyu.edu>
2025-11-27 12:38:11 +0200

    add custom progress bar

---
+++ lib/components/custom-progress-ring/custom-progress-ring.component.d.ts
@@ -0,0 +1,8 @@
+import * as i0 from "@angular/core";
+export declare class CustomProgressRingComponent {
+    value: number;
+    total: number;
+    round(num: number, decPoints: number): number;
+    static ɵfac: i0.ɵɵFactoryDeclaration<CustomProgressRingComponent, never>;
+    static ɵcmp: i0.ɵɵComponentDeclaration<CustomProgressRingComponent, "custom-progress-ring", never, { "value": { "alias": "value"; "required": true; }; "total": { "alias": "total"; "required": true; }; }, {}, never, never, true, never>;
+}
