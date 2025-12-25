ac92d2e54ffb8546b5450a23026e9dccd49c46cf
Fatema <fan6236@nyu.edu>
2025-12-25 19:04:39 +0200

    feat: implement AfterViewInit in CustomInputComponent to focus input at the end on initialization

--- lib/components/custom-input/custom-input.component.d.ts
+++ lib/components/custom-input/custom-input.component.d.ts
@@ -1,6 +1,6 @@
-import { EventEmitter } from '@angular/core';
+import { AfterViewInit, ElementRef, EventEmitter } from '@angular/core';
 import * as i0 from "@angular/core";
-export declare class CustomInputComponent {
+export declare class CustomInputComponent implements AfterViewInit {
     class: string;
     labelClass: string;
     label: string;
@@ -11,6 +11,9 @@ export declare class CustomInputComponent {
     valueChange: EventEmitter<any>;
     showConfirmButton: boolean;
     confirmLabel: string;
+    inputEl: ElementRef<HTMLInputElement>;
+    ngAfterViewInit(): void;
+    private focusAtEnd;
     onConfirm(): void;
     static ɵfac: i0.ɵɵFactoryDeclaration<CustomInputComponent, never>;
     static ɵcmp: i0.ɵɵComponentDeclaration<CustomInputComponent, "custom-input", never, { "class": { "alias": "class"; "required": false; }; "labelClass": { "alias": "labelClass"; "required": false; }; "label": { "alias": "label"; "required": false; }; "placeholder": { "alias": "placeholder"; "required": false; }; "name": { "alias": "name"; "required": false; }; "type": { "alias": "type"; "required": false; }; "value": { "alias": "value"; "required": true; }; "showConfirmButton": { "alias": "showConfirmButton"; "required": false; }; "confirmLabel": { "alias": "confirmLabel"; "required": false; }; }, { "valueChange": "valueChange"; }, never, never, true, never>;
